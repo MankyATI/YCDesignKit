038720149a83fb9bcd6e1d04399d49651f52c1b5
consultant-mayank.pahuja <consultant-mayank.pahuja@mcb.mu>
2022-04-27 23:23:07 +0530

    Initial Commit

---
+++ YCDesignKit/YCDesignKit.h
@@ -0,0 +1,18 @@
+//
+//  YCDesignKit.h
+//  YCDesignKit
+//
+//  Created by Mayank Pahuja on 27/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YCDesignKit.
+FOUNDATION_EXPORT double YCDesignKitVersionNumber;
+
+//! Project version string for YCDesignKit.
+FOUNDATION_EXPORT const unsigned char YCDesignKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YCDesignKit/PublicHeader.h>
+
+
